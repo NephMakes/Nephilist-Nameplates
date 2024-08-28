e23b4424563a433cce7c80f655f7394ab6133b15
NephMakes <37935778+NephMakes@users.noreply.github.com>
2024-08-28 10:10:02 -0500

    CastBar refactor

--- CastBar.lua
+++ CastBar.lua
@@ -4,9 +4,9 @@ local _, NephilistNameplates = ...
 local CastBar = NephilistNameplates.CastBar
 
 -- Constants
-local ALPHA_STEP = 0.05
-local FLASH_STEP = 0.2
-local HOLD_TIME = 1
+local ALPHA_STEP = 0.05  -- Fade out over 20 frames
+local FLASH_STEP = 0.2  -- Flash over 5 frames
+local HOLD_TIME = 1  -- Seconds to show failed/interrupted cast
 local FAILED = FAILED  -- Blizz localized string
 local INTERRUPTED = INTERRUPTED  -- Blizz localized string
 
@@ -25,27 +25,28 @@ function CastBar:OnLoad()
 	self:SetScript("OnUpdate", self.OnUpdate)
 
 	self.startCastColor = CreateColor(0.6, 0.6, 0.6)
-	self.startChannelColor = CreateColor(0.6, 0.6, 0.6)
 	self.finishedCastColor = CreateColor(0.7, 0.7, 0.7)
-	self.failedCastColor = CreateColor(0.5, 0.2, 0.2)
-	self.nonInterruptibleColor = CreateColor(0.3, 0.3, 0.3)
+	self.failedCastColor = CreateColor(0.5, 0.1, 0.1)
+	self.notInterruptibleColor = CreateColor(0.4, 0.4, 0.4)
 	self.Flash:SetVertexColor(1, 1, 1)
 
 	self:SetUnit(nil)
-	self.notInterruptible = false
 end
 
 function CastBar:SetUnit(unit)
+	-- Primary update call
 	-- Called by CastBar:OnLoad, NephilistNameplates.UnitFrame:UpdateCastBar
 	if self.unit == unit then return end  -- Job's done
+
 	self.unit = unit
 	self.casting = nil
 	self.channeling = nil
 	self.holdTime = 0
 	self.fadeOut = nil
+
 	if unit then
 		self:RegisterEvents(unit)
-		self:OnEvent("PLAYER_ENTERING_WORLD")
+		self:PLAYER_ENTERING_WORLD(unit)
 	else
 		self:UnregisterEvents()
 		self:Hide()
@@ -54,101 +55,53 @@ end
 
 function CastBar:RegisterEvents(unit)
 	self:RegisterEvent("PLAYER_ENTERING_WORLD")
-	self:RegisterEvent("UNIT_SPELLCAST_INTERRUPTED")
-	self:RegisterEvent("UNIT_SPELLCAST_INTERRUPTIBLE")
-	self:RegisterEvent("UNIT_SPELLCAST_NOT_INTERRUPTIBLE")
-	self:RegisterEvent("UNIT_SPELLCAST_DELAYED")
-	self:RegisterEvent("UNIT_SPELLCAST_CHANNEL_START")
-	self:RegisterEvent("UNIT_SPELLCAST_CHANNEL_UPDATE")
-	self:RegisterEvent("UNIT_SPELLCAST_CHANNEL_STOP")
 	self:RegisterUnitEvent("UNIT_SPELLCAST_START", unit)
 	self:RegisterUnitEvent("UNIT_SPELLCAST_STOP", unit)
 	self:RegisterUnitEvent("UNIT_SPELLCAST_FAILED", unit)
+	self:RegisterEvent("UNIT_SPELLCAST_CHANNEL_START")
+	self:RegisterEvent("UNIT_SPELLCAST_CHANNEL_UPDATE")
+	self:RegisterEvent("UNIT_SPELLCAST_CHANNEL_STOP")
+	self:RegisterEvent("UNIT_SPELLCAST_DELAYED")
+	self:RegisterEvent("UNIT_SPELLCAST_INTERRUPTED")
+	self:RegisterEvent("UNIT_SPELLCAST_INTERRUPTIBLE")
+	self:RegisterEvent("UNIT_SPELLCAST_NOT_INTERRUPTIBLE")
 end
 
 function CastBar:UnregisterEvents()
 	self:UnregisterEvent("PLAYER_ENTERING_WORLD")
-	self:UnregisterEvent("UNIT_SPELLCAST_INTERRUPTED")
-	self:UnregisterEvent("UNIT_SPELLCAST_INTERRUPTIBLE")
-	self:UnregisterEvent("UNIT_SPELLCAST_NOT_INTERRUPTIBLE")
-	self:UnregisterEvent("UNIT_SPELLCAST_DELAYED")
-	self:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_START")
-	self:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_UPDATE")
-	self:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_STOP")
 	self:UnregisterEvent("UNIT_SPELLCAST_START")
 	self:UnregisterEvent("UNIT_SPELLCAST_STOP")
 	self:UnregisterEvent("UNIT_SPELLCAST_FAILED")
+	self:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_START")
+	self:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_UPDATE")
+	self:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_STOP")
+	self:UnregisterEvent("UNIT_SPELLCAST_DELAYED")
+	self:UnregisterEvent("UNIT_SPELLCAST_INTERRUPTED")
+	self:UnregisterEvent("UNIT_SPELLCAST_INTERRUPTIBLE")
+	self:UnregisterEvent("UNIT_SPELLCAST_NOT_INTERRUPTIBLE")
 end
 
 function CastBar:OnShow()
+	-- Update bar value
 	if not self.unit then return end
 	if self.casting then
 		local _, _, _, startTime = UnitCastingInfo(self.unit)
 		if startTime then
-			self.value = GetTime() - (startTime / 1000)
+			self.value = GetTime() - (startTime/1000)
 		end
 	else
 		local _, _, _, _, endTime = UnitChannelInfo(self.unit)
 		if endTime then
-			self.value = (endTime / 1000) - GetTime()
+			self.value = (endTime/1000) - GetTime()
 		end
 	end
 end
 
-function CastBar:GetEffectiveStartColor(isChannel)
-	return (isChannel and self.startChannelColor) or self.startCastColor
-end
-
-function CastBar:OnEvent(event, ...)
-	local arg1 = ...
-	local unit = self.unit
-	if arg1 ~= unit then return end
-
+function CastBar:OnEvent(event, unit, ...)
+	if unit ~= self.unit then return end
 	local eventFunction = self[event]
 	if eventFunction then
 		eventFunction(self, unit, ...)
-	end
-
-	if event == "UNIT_SPELLCAST_STOP" or event == "UNIT_SPELLCAST_CHANNEL_STOP" then
-		if not self:IsVisible() then
-			self:Hide()
-			return
-		end
-		if (self.casting and event == "UNIT_SPELLCAST_STOP" and select(2, ...) == self.castID) or
-			(self.channeling and event == "UNIT_SPELLCAST_CHANNEL_STOP") 
-		then
-			self.Spark:Hide()
-			self.Flash:SetAlpha(0)
-			self.Flash:Show()
-			self:SetValue(self.maxValue)
-			if event == "UNIT_SPELLCAST_STOP" then
-				self.casting = nil
-				self:SetStatusBarColor(self.finishedCastColor:GetRGB())
-			else
-				self.channeling = nil
-			end
-			self.flash = true
-			self.fadeOut = true
-			self.holdTime = 0
-		end
-	elseif event == "UNIT_SPELLCAST_FAILED" or event == "UNIT_SPELLCAST_INTERRUPTED" then
-		if self:IsShown() and 
-			(self.casting and select(2, ...) == self.castID) and 
-			not self.fadeOut
-		then
-			self:SetValue(self.maxValue)
-			self:SetStatusBarColor(self.failedCastColor:GetRGB())
-			self.Spark:Hide()
-			if event == "UNIT_SPELLCAST_FAILED" then
-				self.Text:SetText(FAILED)
-			else
-				self.Text:SetText(INTERRUPTED)
-			end
-			self.casting = nil
-			self.channeling = nil
-			self.fadeOut = true
-			self.holdTime = GetTime() + HOLD_TIME
-		end
 	end
 end
 
@@ -167,44 +120,62 @@ end
 
 function CastBar:UNIT_SPELLCAST_START(unit)
 	local name, text, _, startTime, endTime, _, castID, notInterruptible = UnitCastingInfo(unit)
-	self.notInterruptible = notInterruptible
-
 	if not name then
 		self:Hide()
 		return
 	end
 
-	local startColor = self:GetEffectiveStartColor(false)
-	self:SetStatusBarColor(startColor:GetRGB())
-	
-	self.Spark:Show()
 	self.value = GetTime() - (startTime / 1000)
 	self.maxValue = (endTime - startTime) / 1000
 	self:SetMinMaxValues(0, self.maxValue)
 	self:SetValue(self.value)
-	self.Text:SetText(text)
-	self:SetAlpha(1)
 
-	self.holdTime = 0
+	local startColor = self:GetStartColor(notInterruptible)
+	self:SetStatusBarColor(startColor:GetRGB())
+	self.Text:SetText(text)
+	self.Spark:Show()
+	self:ShowNotInterruptible(notInterruptible)
+
 	self.casting = true
-	self.castID = castID
 	self.channeling = nil
+	self.castID = castID
+	self.holdTime = 0
 	self.fadeOut = nil
 
-	if notInterruptible then
-		self.BorderShield:Show()
-	else
-		self.BorderShield:Hide()
+	self:SetAlpha(1)
+	self:Show()
+end
+
+function CastBar:UNIT_SPELLCAST_CHANNEL_START(unit)
+	local name, text, _, startTime, endTime, _, notInterruptible, _ = UnitChannelInfo(unit)
+	if not name then
+		self:Hide()
+		return
 	end
+
+	self.value = (endTime / 1000) - GetTime()
+	self.maxValue = (endTime - startTime) / 1000
+	self:SetMinMaxValues(0, self.maxValue)
+	self:SetValue(self.value)
+
+	local startColor = self:GetStartColor(notInterruptible)
+	self:SetStatusBarColor(startColor:GetRGB())
+	self.Text:SetText(text)
+	self.Spark:Hide()
+	self:ShowNotInterruptible(notInterruptible)
+
+	self.casting = nil
+	self.channeling = true
+	self.holdTime = 0
+	self.fadeOut = nil
+
+	self:SetAlpha(1)
 	self:Show()
 end
 
 function CastBar:UNIT_SPELLCAST_DELAYED(unit)
 	if not self:IsShown() then return end
-
 	local name, _, _, startTime, endTime, _, _, notInterruptible = UnitCastingInfo(unit)
-	self.notInterruptible = notInterruptible
-
 	if not name then
 		self:Hide()
 		return
@@ -213,48 +184,20 @@ function CastBar:UNIT_SPELLCAST_DELAYED(unit)
 	self.value = GetTime() - (startTime / 1000)
 	self.maxValue = (endTime - startTime) / 1000
 	self:SetMinMaxValues(0, self.maxValue)
+	self:SetValue(self.value)
+
 	if not self.casting then
-		self:SetStatusBarColor(self:GetEffectiveStartColor(false):GetRGB())
+		local startColor = self:GetStartColor(notInterruptible)
+		self:SetStatusBarColor(startColor:GetRGB())
 		self.Spark:Show()
 		self.Flash:SetAlpha(0)
 		self.Flash:Hide()
+
 		self.casting = true
 		self.channeling = nil
 		self.flash = nil
 		self.fadeOut = nil
 	end
-end
-
-function CastBar:UNIT_SPELLCAST_CHANNEL_START(unit)
-	local name, text, _, startTime, endTime, _, notInterruptible, _ = UnitChannelInfo(unit)
-	self.notInterruptible = notInterruptible
-
-	if not name then
-		self:Hide()
-		return
-	end
-
-	local startColor = self:GetEffectiveStartColor(true)
-	self:SetStatusBarColor(startColor:GetRGB())
-	self.value = (endTime / 1000) - GetTime()
-	self.maxValue = (endTime - startTime) / 1000
-	self:SetMinMaxValues(0, self.maxValue)
-	self:SetValue(self.value)
-	self.Text:SetText(text)
-	self.Spark:Hide()
-	self:SetAlpha(1)
-
-	self.holdTime = 0
-	self.casting = nil
-	self.channeling = true
-	self.fadeOut = nil
-
-	if notInterruptible then
-		self.BorderShield:Show()
-	else
-		self.BorderShield:Hide()
-	end
-	self:Show()
 end
 
 function CastBar:UNIT_SPELLCAST_CHANNEL_UPDATE(unit)
@@ -270,23 +213,99 @@ function CastBar:UNIT_SPELLCAST_CHANNEL_UPDATE(unit)
 	self:SetValue(self.value)
 end
 
+function CastBar:UNIT_SPELLCAST_STOP(unit, castID)
+	if not self:IsVisible() then
+		self:Hide()
+		return
+	end
+	if self.casting and (castID == self.castID) then
+		self:SetValue(self.maxValue)
+		self:SetStatusBarColor(self.finishedCastColor:GetRGB())
+		self.Spark:Hide()
+		self.Flash:Show()
+		self.Flash:SetAlpha(0)
+
+		self.casting = nil
+		self.holdTime = 0
+		self.flash = true
+		self.fadeOut = true
+	end
+end
+
+function CastBar:UNIT_SPELLCAST_CHANNEL_STOP(unit)
+	if not self:IsVisible() then
+		self:Hide()
+		return
+	end
+	if self.channeling then
+		self:SetValue(self.maxValue)
+		self.Spark:Hide()
+		self.Flash:Show()
+		self.Flash:SetAlpha(0)
+
+		self.channeling = nil
+		self.holdTime = 0
+		self.flash = true
+		self.fadeOut = true
+	end
+end
+
+function CastBar:UNIT_SPELLCAST_FAILED(unit, castID)
+	if self:IsShown() and 
+		(self.casting and castID == self.castID) and 
+		not self.fadeOut
+	then
+		self:SetValue(self.maxValue)
+		self:SetStatusBarColor(self.failedCastColor:GetRGB())
+		self.Spark:Hide()
+		self.Text:SetText(FAILED)
+		self.casting = nil
+		self.channeling = nil
+		self.fadeOut = true
+		self.holdTime = GetTime() + HOLD_TIME
+	end
+end
+
+function CastBar:UNIT_SPELLCAST_INTERRUPTED(unit, castID)
+	if self:IsShown() and 
+		(self.casting and castID == self.castID) and 
+		not self.fadeOut
+	then
+		self:SetValue(self.maxValue)
+		self:SetStatusBarColor(self.failedCastColor:GetRGB())
+		self.Spark:Hide()
+		self.Text:SetText(INTERRUPTED)
+		self.casting = nil
+		self.channeling = nil
+		self.fadeOut = true
+		self.holdTime = GetTime() + HOLD_TIME
+	end
+end
+
 function CastBar:UNIT_SPELLCAST_INTERRUPTIBLE()
-	self:UpdateInterruptible(true)
+	CastBar:ShowNotInterruptible(false)
+	self:SetStatusBarColor(self.startCastColor:GetRGB())
 end
 
 function CastBar:UNIT_SPELLCAST_NOT_INTERRUPTIBLE()
-	self:UpdateInterruptible(false)
+	CastBar:ShowNotInterruptible(true)
+	self:SetStatusBarColor(self.notInterruptibleColor:GetRGB())
 end
 
-function CastBar:UpdateInterruptible(isInterruptible)
-	if self.casting or self.channeling then
-		local startColor = self:GetEffectiveStartColor(self.channeling)
-		self:SetStatusBarColor(startColor:GetRGB())
-		if isInterruptible then
-			self.BorderShield:Hide()
-		else
-			self.BorderShield:Show()
-		end
+function CastBar:GetStartColor(notInterruptible)
+	if notInterruptible then
+		return self.notInterruptibleColor
+	else
+		return self.startCastColor
+	end
+end
+
+function CastBar:ShowNotInterruptible(notInterruptible)
+	-- Show/hide icon indicating not interruptible
+	if notInterruptible then
+		self.BorderShield:Show()
+	else
+		self.BorderShield:Hide()
 	end
 end
 
@@ -299,9 +318,9 @@ function CastBar:OnUpdate(elapsed)
 			return
 		end
 		self:SetValue(self.value)
-		self.Flash:Hide()
 		local sparkPosition = (self.value / self.maxValue) * self:GetWidth()
 		self.Spark:SetPoint("CENTER", self, "LEFT", sparkPosition, -1)
+		self.Flash:Hide()
 	elseif self.channeling then
 		self.value = self.value - elapsed
 		if self.value <= 0 then
@@ -335,10 +354,10 @@ end
 function CastBar:FinishSpell()
 	self:SetStatusBarColor(self.finishedCastColor:GetRGB())
 	self.Spark:Hide()
-	self.Flash:SetAlpha(0)
 	self.Flash:Show()
-	self.flash = true
-	self.fadeOut = true
+	self.Flash:SetAlpha(0)
 	self.casting = nil
 	self.channeling = nil
+	self.flash = true
+	self.fadeOut = true
 end
